d428d1b95652a6823e53d5bbd27c809eb332ca26
Xavier Portilla Edo <xavi_tb@hotmail.com>
2021-01-26 11:11:58 +0100

    fix: move pr check to circleci and eslint finished

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,3 @@
 module.exports = {
   extends: '@voiceflow/eslint-config',
-  rules: {
-    'no-continue': 'off',
-    'quotes': ['error', 'single', 'avoid-escape'],
-    'sonarjs/cognitive-complexity': 'warn',
-    'promise/always-return': 'off',
-  }
 };
