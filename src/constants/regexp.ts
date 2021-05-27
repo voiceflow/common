f59a72e4950a05035d0df256bc05016b91973dd3
Ben Teichman <ben@voiceflow.com>
2021-05-27 16:06:39 -0400

    build: update tooling

--- src/constants/regexp.ts
+++ src/constants/regexp.ts
@@ -4,7 +4,7 @@ export const SLOT_REGEXP = /{{\[([^ .[\]{}]*?)]\.([^ .[\]{}]*?)}}/g;
 
 export const IS_VARIABLE_REGEXP = /^{.*}$/;
 
-export const READABLE_VARIABLE_REGEXP = /\{([a-zA-Z0-9_]{1,64})\}/g;
+export const READABLE_VARIABLE_REGEXP = /{(\w{1,64})}/g;
 
 export const VALID_CHARACTER = 'a-zA-Z';
 
