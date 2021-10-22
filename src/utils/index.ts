578f7815dac1cfa8a8361ba3fbffa32788fb3ed8
Kevin Wolf <hi@kevinwolf.dev>
2021-10-22 12:27:58 -0600

    feat: move common utilities to @voiceflow/common (VF-1979) (#70)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,19 @@
+export * as emails from './emails';
+export * as functional from './functional';
+export * as generate from './generate';
+export * as id from './id';
+export * as intent from './intent';
+export * as mathjs from './mathjs';
+export * as normalized from './normalized';
+export * as number from './number';
+export * as object from './object';
+export * as promise from './promise';
+export * as string from './string';
+export * as time from './time';
+export * as timezones from './timezones';
+export * as variables from './variables';
+
+// TODO: Remove these exports once we migrate all utils in other repos.
 export * from './generate';
 export * from './intent';
 export * from './variables';
---
+++ typings/number-to-words.d.ts
@@ -0,0 +1,6 @@
+declare module 'number-to-words/src' {
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  import * as NumberToWords from 'number-to-words';
+
+  export = NumberToWords;
+}
