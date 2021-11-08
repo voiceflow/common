c1cb0c85176269c589c36efbf51eeb0b80de46ce
Ben Teichman <ben@voiceflow.com>
2021-11-08 13:37:14 -0500

    feat: add custom en language mapping for dayjs

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -4,6 +4,7 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 import duration from 'dayjs/plugin/duration';
 import minMax from 'dayjs/plugin/minMax';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import updateLocale from 'dayjs/plugin/updateLocale';
 import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(utc);
@@ -12,6 +13,26 @@ dayjs.extend(relativeTime);
 dayjs.extend(customParseFormat);
 dayjs.extend(minMax);
 dayjs.extend(advancedFormat);
+dayjs.extend(updateLocale);
+
+// https://day.js.org/docs/en/customization/relative-time
+dayjs.updateLocale('en', {
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: 'seconds',
+    m: 'a minute',
+    mm: '%d minutes',
+    h: 'an hour',
+    hh: '%d hours',
+    d: 'a day',
+    dd: '%d days',
+    M: 'a month',
+    MM: '%d months',
+    y: 'a year',
+    yy: '%d years',
+  },
+});
 
 export const getCurrentTimestamp = () => Math.floor(Date.now() / 1000);
 
